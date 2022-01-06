26d5e43ffca9e21d0c9fc38008fa501785b00f27
andreMapa <andreomapa01@gmail.com>
2022-01-06 10:35:07 -0300

    SellerJDBc and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,19 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		SellerDao sellerdao = DaoFactory.creatSellerDao();
+		
+		System.out.println("=== TEST 1: seller findById ===");
+		Seller seller = sellerdao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
