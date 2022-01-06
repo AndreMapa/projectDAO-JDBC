1dd9c22d5834b27fbe29966ee38eae6d3cc30a45
André Mapa <85127170+AndreMapa@users.noreply.github.com>
2022-01-06 14:36:16 -0300

    Update Program2.java

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -15,28 +15,28 @@ public class Program2 {
 		
 		DepartmentDao departmentDao = DaoFactory.creatDepartmentDao();
 		
-		System.out.println("=== TEST 1: seller findById ===");
+		System.out.println("=== TEST 1: department findById ===");
 		Department department = departmentDao.findById(2);
 		System.out.println(department);
 		
-		System.out.println("\n=== TEST 2: seller findAll ===");
+		System.out.println("\n=== TEST 2: department findAll ===");
 		List<Department> list = departmentDao.findAll();
 		for(Department obj: list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 3: seller insert ===");
+		System.out.println("\n=== TEST 3: department insert ===");
 		Department newDepartment = new Department(null, "Music");
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New id = " + newDepartment.getId());
 		
-		System.out.println("\n=== TEST 4: seller update ===");
+		System.out.println("\n=== TEST 4: department update ===");
 		department = departmentDao.findById(5);
 		department.setName("Art");
 		departmentDao.update(department); 
 		System.out.println("Update completed!");
 		
-		System.out.println("\n=== TEST 5: seller deleteById ===");
+		System.out.println("\n=== TEST 5: department deleteById ===");
 		System.out.print("Enter id for delete test: ");
 		int id = sc.nextInt();
 		departmentDao.deleteById(id);
